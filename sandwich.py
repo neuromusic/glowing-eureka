f1b022e3ad4b2d1ec05758b81429e4f83b6d89a8
Justin Kiggins <justink@alleninstitute.org>
2017-06-15 11:45:18 -0700

    leave if already sandwich

--- sandwich.py
+++ sandwich.py
@@ -1,3 +1,6 @@
 
 def sandwichify(any_string):
-    return '{}_sandwich'.format(any_string)
+    if '_sandwich' in any_string:
+        return any_string
+    else:
+        return '{}_sandwich'.format(any_string)
